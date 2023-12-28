7e9ee675ce775fbf91dea1aaaddcc5b04feb3241
李小飞 <lixf@geovis.com.cn>
2023-12-28 13:43:43 +0800

    update

--- src/containers/TheFooter.vue
+++ src/containers/TheFooter.vue
@@ -1,14 +1,10 @@
 <template>
   <CFooter :fixed="false">
-    <div>
-        <a href="https://strapiadmin.lixf.ink/" target="_blank">StrApi Admin</a>
-        <span class="ml-1">&copy; {{ new Date().getFullYear() }}</span>
-      </div>
     <div class="mfs-auto">
-        <span class="mr-1">后台由</span>
-        <a href="https://strapi.io/">StrApi</a>
-        <span class="mr-1">驱动</span>
-      </div>
+      <span class="mr-1">后台由</span>
+      <a href="https://strapi.io/">StrApi</a>
+      <span class="mr-1">驱动</span>
+    </div>
   </CFooter>
 </template>
 
